83e88d0ffa055ef6880f29eda8a01a0a34445124
Thiago*-Tico <thiago.tec.costa@gmail.com>
2024-10-31 16:13:55 +0100

    Altera o Server

--- src/server.js
+++ src/server.js
@@ -18,8 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas vindas a API do AllBooks")
-  console.log("API disponível atraves da URL http://localhost:8000")
-  console.log("Acesse essa API em http://localhost:8000")
+  console.log("API disponivel em http://localhost:8000")
 })
